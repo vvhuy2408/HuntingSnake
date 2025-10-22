6206ac4fd6a3bb9512ab0bb39039d36f5dcefa00
mthu <pmthuwork152@gmail.com>
2025-10-22 08:00:20 +0700

    mhu: chỉnh tùm lum tá lả

---
+++ build/CMakeFiles/HuntingSnake.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HuntingSnake.dir/Design/effects.cpp.o"
+  "CMakeFiles/HuntingSnake.dir/Design/effects.cpp.o.d"
+  "CMakeFiles/HuntingSnake.dir/Design/interface.cpp.o"
+  "CMakeFiles/HuntingSnake.dir/Design/interface.cpp.o.d"
+  "CMakeFiles/HuntingSnake.dir/Design/textureManager.cpp.o"
+  "CMakeFiles/HuntingSnake.dir/Design/textureManager.cpp.o.d"
+  "CMakeFiles/HuntingSnake.dir/main.cpp.o"
+  "CMakeFiles/HuntingSnake.dir/main.cpp.o.d"
+  "HuntingSnake"
+  "HuntingSnake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HuntingSnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
