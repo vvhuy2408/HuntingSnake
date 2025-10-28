f86cc3ed47d5ce236a099658102fa52dff90b8b1
mthu <pmthuwork152@gmail.com>
2025-10-28 17:04:05 +0700

    mthu chỉnh sửa một chút

--- Design/interface.hpp
+++ Design/interface.hpp
@@ -5,6 +5,25 @@
 #include <SFML/Graphics.hpp>
 #include "textureManager.hpp"
 
-sf::Sprite loadInterface(const std::string& path, const std::string& filename, const sf::RenderWindow& window);
+struct Button
+{
+    sf::Sprite sprite;
+    sf::Texture normalTexture;
+    sf::Texture hoverTexture;
+    bool hasHoverTexture = false;
+
+    bool isHovered = false;
+    bool isClicked = false;
+    bool isPressed = false;
+
+    sf::Vector2f originalPos;
+    sf::Vector2f hoverOffset = {0.f, 0.f};
+};
+
+Button createButton(const std::string &normalPath, const std::string &hoverPath, float x, float y);
+void updateButton(Button &btn, sf::RenderWindow &window);
+void drawButton(sf::RenderWindow &window, Button &btn);
+
+sf::Sprite loadInterface(const std::string &path, const std::string &filename, const sf::RenderWindow &window);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,36 +1,76 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+#include <stack>
 #include <iostream>
 
 #include "Design/textureManager.hpp"
 #include "Design/effects.hpp"
 #include "Design/interface.hpp"
+#include "Design/screenState.hpp"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Hunting Snake");
 
-    // Background
+    // Backgrounds
     sf::Sprite start_game = loadInterface("Design/Assets/startgame.png", "start-game", window);
-    sf::Sprite main_menu = loadInterface("Design/Assets/mainmenu.png", "main-menu", window);
+    sf::Sprite main_menu  = loadInterface("Design/Assets/mainmenu.png", "main-menu", window);
+    sf::Sprite in_game    = loadInterface("Design/Assets/ingame.png", "in-game", window);
 
     // Buttons
-    Button start_button = creatButton("Design/Assets/button/start.png", "start-game-button", 640, 550);
-    Button soundOn_button = creatButton("Design/Assets/button/sound_on.png", "soundOn-game-button", 48, 48);
-    Button soundOff_button = creatButton("Design/Assets/button/sound_off.png", "soundOff-game-button", 48, 48);
+    Button start_button    = createButton("Design/Assets/button/start.png", "", 640, 550);
+    Button soundOn_button  = createButton("Design/Assets/button/sound_on.png", "", 48, 48);
+    Button soundOff_button = createButton("Design/Assets/button/sound_off.png", "", 48, 48);
+    Button back_button     = createButton("Design/Assets/button/arrow_left.png", "", 148, 48);
+    Button heart_button    = createButton("Design/Assets/button/heart_button.png", "", 500, 428);
+    Button loadgame_button = createButton("Design/Assets/button/loadgame.png", "Design/Assets/button/loadgame_press.png", 980, 169);
+    Button howto_button    = createButton("Design/Assets/button/howto.png",   "Design/Assets/button/howto_press.png",   980, 369);
+    Button about_button    = createButton("Design/Assets/button/about.png",   "Design/Assets/button/about_press.png",   980, 569);
 
-    bool isExit = false;
-    bool inGame = false;
+    loadgame_button.hoverOffset = sf::Vector2f(35.f, 0.f);
+    howto_button.hoverOffset    = sf::Vector2f(35.f, 0.f);
+    about_button.hoverOffset    = sf::Vector2f(35.f, 0.f);
+
+    // Music
+    sf::Music backgroundMusic;
+    if (!backgroundMusic.openFromFile("Design/Music/background.ogg"))
+        std::cout << "Error loading background music!\n";
+    else 
+    {
+        backgroundMusic.setLoop(true);
+        backgroundMusic.setVolume(60);
+        backgroundMusic.play();
+    }
+
+    bool isExit   = false;
     bool soundOff = false;
 
+    // Initialize the screen stack once
+    std::stack<ScreenState> screenStack;
+    screenStack.push(ScreenState::StartMenu);
+
+    // Font
+    sf::Font font;
+    if (!font.loadFromFile("Design/Assets/Font/ThaleahFat.ttf")) 
+    {
+        std::cout << "Error loading font!\n";
+    }
+
+    // Clock
+    sf::Clock clock;
+    sf::Time duration01 = sf::seconds(30); // input thời gian ở đây nhé
+
+
+    // =================== MAIN LOOP ===================
     while (window.isOpen())
     {
-
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 isExit = true;
-
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
                 isExit = true;
         }
@@ -43,68 +83,88 @@ int main()
             continue;
         }
 
-        if (inGame)
-        {
-            window.clear();
-            window.draw(main_menu);
-            if (!soundOff)
-            {
-                drawButton(window, soundOn_button);
-                updateButton(soundOn_button, window);
-            }
-            else
-            {
-                drawButton(window, soundOff_button);
-            }
-            window.display();
-
-            if (soundOff)
-            {
-                drawButton(window, soundOff_button);
-                updateButton(soundOff_button, window);
-                if (soundOff_button.isClicked)
-                    soundOff = false;
-            }
-            else
-            {
-                drawButton(window, soundOn_button);
-                updateButton(soundOn_button, window);
-                if (soundOn_button.isClicked)
-                    soundOff = true;
-            }
-            continue;
-        }
-
-        updateButton(start_button, window);
-
-        if (soundOff)
-        {
-            drawButton(window, soundOff_button);
-            updateButton(soundOff_button, window);
-            if (soundOff_button.isClicked)
-                soundOff = false;
-        }
-        else
-        {
-            drawButton(window, soundOn_button);
-            updateButton(soundOn_button, window);
-            if (soundOn_button.isClicked)
-                soundOff = true;
-        }
-
-        if (start_button.isClicked)
-        {
-            inGame = true;
-        }
-
         window.clear();
-        window.draw(start_game);
-        drawButton(window, start_button);
+        ScreenState currentScreen = screenStack.top();
 
+        // =================== SWITCH BETWEEN SCREENS ===================
+        switch (currentScreen)
+        {
+        case ScreenState::StartMenu:
+            window.draw(start_game);
+            updateButton(start_button, window);
+            drawButton(window, start_button);
+
+            if (start_button.isClicked)
+                changeScreen(screenStack, ScreenState::MainMenu);
+            break;
+
+        case ScreenState::MainMenu:
+            window.draw(main_menu);
+
+            updateButton(back_button, window);
+            drawButton(window, back_button);
+
+            updateButton(heart_button, window);
+            drawButton(window, heart_button);
+
+            updateButton(loadgame_button, window);
+            drawButton(window, loadgame_button);
+
+            updateButton(howto_button, window);
+            drawButton(window, howto_button);
+
+            updateButton(about_button, window);
+            drawButton(window, about_button);
+
+            if (back_button.isClicked)
+                goBack(screenStack);
+
+            if (howto_button.isClicked)
+                changeScreen(screenStack, ScreenState::HowTo);
+                
+            if (about_button.isClicked)
+                changeScreen(screenStack, ScreenState::About);
+            
+            if (heart_button.isClicked)
+                changeScreen(screenStack, ScreenState::InGame);
+        
+            if (loadgame_button.isClicked)
+                changeScreen(screenStack, ScreenState::LoadGame);
+            break;
+
+        case ScreenState::InGame:
+            window.draw(in_game);
+            updateButton(back_button, window);
+            drawButton(window, back_button);
+
+            if (back_button.isClicked)
+                goBack(screenStack);
+
+            break;
+
+        default:
+            break;
+        }
+
+        // =================== SOUND TOGGLE (GLOBAL) ===================
         if (soundOff)
+        {
+            updateButton(soundOff_button, window);
             drawButton(window, soundOff_button);
+            if (soundOff_button.isClicked) {
+                soundOff = false;
+                backgroundMusic.play();
+            }
+        }
         else
+        {
+            updateButton(soundOn_button, window);
             drawButton(window, soundOn_button);
+            if (soundOn_button.isClicked) {
+                soundOff = true;
+                backgroundMusic.pause();
+            }
+        }
 
         window.display();
     }
